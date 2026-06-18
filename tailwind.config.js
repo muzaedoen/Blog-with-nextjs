9b02d4cbd9c7b15bad0855f4e6b0430df15ac2fb
muzaedoen <90777694+muzaedoen@users.noreply.github.com>
2026-06-18 14:23:16 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'media', // Otomatis Dark Mode mengikuti setelan HP/Browser user
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        // Kita kunci warna biru premium khusus untuk blogmu
-        brand: {
-          50: '#f0f7ff',
-          100: '#e0effe',
-          500: '#0284c7',
-          600: '#0369a1',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
